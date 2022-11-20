ea6da057781c39791083cf186b5c295defdb9e98
Firman Wahyudi <e41190305@student.polije.ac.id>
2022-11-20 10:52:43 +0700

    up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,91 +1,66 @@
-@extends('layouts.app')
+@include('admin.partials.head')
 
-@section('content')
+<body class="bg-gradient-primary">
 
-<div class="auth-container d-flex">
+    <div class="container">
 
-    <div class="container mx-auto align-self-center">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
 
-        <div class="row">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
-            <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center mx-auto">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <div class="card mt-3 mb-3">
-                        <div class="card-body">
-
-                            <div class="row">
-                                <div class="col-md-12 mb-3">
-
-                                    <h2>Sign In</h2>
-                                    <p>Enter your email and password to login</p>
-
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="mb-3">
-                                        <label class="form-label">Email</label>
-                                        <input id="email" type="email"
-                                            class="form-control @error('email') is-invalid @enderror" name="email"
-                                            value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="mb-4">
-                                        <label class="form-label">Password</label>
-                                        <input id="password" type="password"
-                                            class="form-control @error('password') is-invalid @enderror" name="password"
-                                            required autocomplete="current-password">
-
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="mb-3">
-                                        <div class="form-check form-check-primary form-check-inline">
-                                            <input class="form-check-input me-3" type="checkbox"
-                                                id="form-check-default">
-                                            <label class="form-check-label" for="form-check-default">
-                                                Remember me
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-12">
-                                    <div class="mb-4">
-                                        <button type="submit" class="btn btn-secondary w-100">SIGN IN</button>
-                                    </div>
-                                </div>
-
-
-                                <div class="col-12">
-                                    <div class="text-center">
-                                        <p class="mb-0">Dont't have an account ? <a href="javascript:void(0);"
-                                                class="text-warning">Sign Up</a></p>
-                                    </div>
-                                </div>
-
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block ">
+                                <img class="col-md img-center" src="{{ asset('template/img/mdr-logo.svg') }}">
                             </div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input id="email" type="email"
+                                                class="form-control form-control-user @error('email') is-invalid @enderror"
+                                                name="email" value="{{ old('email') }}" required autocomplete="email"
+                                                autofocus placeholder="Enter  your email address">
 
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input id="password" type="password"
+                                                class="form-control form-control-user @error('password') is-invalid @enderror"
+                                                name="password" required autocomplete="current-password"
+                                                placeholder="Enter your password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            {{ __('Login') }}
+                                        </button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="{{ route('register') }}">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </form>
-
+                </div>
             </div>
-
         </div>
-
     </div>
-
-</div>
-@endsection
+</body>
