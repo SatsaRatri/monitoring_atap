d9bbfd70f167635ba690b9962cfb0b5de66f1b50
Firman Wahyudi <firmangetrich50@gmail.com>
2023-03-29 14:04:42 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use App\Http\Controllers\DataIotController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/sensor', [DataIotController::class, 'datasensor'])->name('datasensor');
+Route::get('/', [DataIotController::class, 'datasensor'])->name('datasensor');
