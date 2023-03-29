66ea6ec99c9bec7fb30bcc2bea0e7fa5d174d61d
Satsa Ratri <bernadeta252@gmail.com>
2023-03-29 13:48:13 +0700

    up

--- resources/views/admin/analisis.blade.php
+++ resources/views/admin/analisis.blade.php
@@ -10,9 +10,9 @@
                         <div class="col-lg-9">
                             <h1 class="text-primary">Apa itu Analisis Preventive Maintenance?</h1>
                             <p class="lead mb-4">Tindakan preventive maintenance agar dapat meningkatkan kinerja dari perusahaan, untuk meminimalisir terjadinya breakdown dan downtime maka perlu adanya sistem penjadwalan perawatan yang baik guna mencegah terjadinya kerusakan mesin. Salah satu metode sebagai acuan untuk menetapkan jadwal perawatan yang efektif, yaitu :</p>
-                            <h4 class="text-primary lead mb-5">1. ANALISIS MEAN TIME TO REPAIR (MTTR)</h4>
+                            <h4 class="text-primary lead mb-3">1. ANALISIS MEAN TIME TO REPAIR (MTTR)</h4>
                             <h6 class="lead mb-4">Mean time to repair (MTTR) adalah jumlah rata-rata waktu yang diperlukan untuk memperbaiki sistem dan mengembalikannya ke fungsionalitas penuh. </h6>
-                            <h4 class="text-primary lead mb-5">2. MEAN TIME BETWEEN FAILURE (MTBF)</h4>
+                            <h4 class="text-primary lead mb-3">2. MEAN TIME BETWEEN FAILURE (MTBF)</h4>
                             <h6 class="lead mb-4">Waktu rata-rata antara kegagalan (MTBF) adalah waktu rata-rata yang dilalui antara kegagalan perangkat keras yang dapat diperbaiki dan waktu berikutnya terjadi. Ketersediaan dan keandalan alat ukur MTBF, sehingga semakin tinggi jumlah MTBF, semakin andal sistemnya.</h6>
                         </div>
                         <div class="col-lg-3"><img class="img-fluid" src="{{ asset('template/img/problem-solving.svg') }}">
@@ -66,7 +66,7 @@
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-3">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"
+                                    <div class="text-sm font-weight-bold text-primary text-uppercase mb-1"
                                         style="text-align:center">Analisis Mean Time To Repair (MTTR)</div>
                                     <div class="mb-3">
                                         <input class="form-control" id="inputnilai" name="nilai" type="text"
@@ -82,7 +82,7 @@
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-3">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1"
+                                    <div class="text-sm font-weight-bold text-primary text-uppercase mb-1"
                                         style="text-align:center">Mean Time Between Failure (MTBF)</div>
                                     <div class="mb-3">
                                         <input class="form-control  col-md-15" id="inputnilai" name="nilai" type="text"
